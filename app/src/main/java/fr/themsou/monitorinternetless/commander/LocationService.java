4b085691380c7c6bd39f0e52345a2b66e0e272e1
Clément Grennerat <clement.grennerat@free.fr>
2023-08-29 15:12:21 +0200

    Setup mixed google localisation and locationManager

--- app/src/main/java/fr/themsou/monitorinternetless/commander/LocationService.java
+++ app/src/main/java/fr/themsou/monitorinternetless/commander/LocationService.java
@@ -15,7 +15,14 @@ import android.os.Looper;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -26,35 +33,75 @@ import fr.themsou.monitorinternetless.R;
 public class LocationService extends Service {
 
     private LocationManager locationManager;
-    private LocationListener locationListener;
-    private static BlockingQueue<Location> locationQueue = new LinkedBlockingQueue<>();
+
+    private final LocationListener locationListener = this::locationReceived;
+
+    private static final BlockingQueue<Location> locationQueue = new LinkedBlockingQueue<>();
 
     private static final int NOTIFICATION_ID = 2;
-    private static final String CHANNEL_ID = "LocationServiceChannel";
+    private static final String CHANNEL_ID = "LocationService";
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        locationQueue.clear();
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        locationListener = location -> {
-            Log.d(CHANNEL_ID, "Location listener called. lat,long:" + location.getLatitude() + "," + location.getLongitude());
-            locationQueue.add(location);
-        };
     }
 
     @SuppressLint("MissingPermission")
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    private void locationReceived(Location location){
+        Log.d(CHANNEL_ID, "Location listener called. lat,long:" + location.getLatitude() + "," + location.getLongitude());
+        locationQueue.add(location);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        stopForeground(true);
+        stopSelf();
+    }
+
+    @SuppressLint("MissingPermission")
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
             startForeground(NOTIFICATION_ID, createNotification(), ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION);
-        }else {
+        }else{
             startForeground(NOTIFICATION_ID, createNotification());
         }
 
-        locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, Looper.getMainLooper());
+        if(isGooglePlayServicesAvailable() && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+            Log.d(CHANNEL_ID, "Google play services available, using fused location provider");
+            requestLocationUsingFusedLocationProviderClient();
+        }else{
+            Log.d(CHANNEL_ID, "Google play services not available, using location manager");
+            requestLocationUsingLocationManager();
+        }
         return START_STICKY;
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestLocationUsingFusedLocationProviderClient(){
+        final FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null).addOnSuccessListener(location -> {
+            Log.d(CHANNEL_ID, "Fused location provider returned location");
+            locationReceived(location);
+
+        }).addOnFailureListener(e -> {
+            Log.d(CHANNEL_ID, "Fused location provider failed to return location, using location manager");
+            requestLocationUsingLocationManager();
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestLocationUsingLocationManager(){
+        locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, Looper.getMainLooper());
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        return resultCode == ConnectionResult.SUCCESS;
     }
 
     private Notification createNotification() {
